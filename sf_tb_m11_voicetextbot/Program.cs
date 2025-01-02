14839417414dba7d2d35faf9daf8bef76967a209
aveA <aveA@AVE>
2025-01-02 13:44:06 +0300

    Добавьте файлы проекта.

---
+++ sf_tb_m11_voicetextbot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sf_tb_m11_voicetextbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
